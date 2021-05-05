6da3fbb62523aa16e6bcb2accd20b97a281ef40e
Austin <harrenaustint@gmail.com>
2021-05-05 11:58:47 -0400

    pushing 1-13

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
 // Instructions: The current code uses JSX to assign a div element to the constant JSX. Replace the div with an h1 element and add the text Hello JSX! inside it.
 
-const JSX = <div></div>;
+const JSX = <h1>Hello JSX!</h1>;
 
 // 2 React: Create a Complex JSX Element
 // The last challenge was a simple example of JSX, but JSX can represent more complex HTML as well.
@@ -26,21 +26,36 @@ const JSX = <div></div>;
 
 // Valid JSX:
 
-// <div>
-//   <p>Paragraph One</p>
-//   <p>Paragraph Two</p>
-//   <p>Paragraph Three</p>
-// </div>
+<div>
+  <p>Paragraph One</p>
+  <p>Paragraph Two</p>
+  <p>Paragraph Three</p>
+</div>
 // Invalid JSX:
 
-// <p>Paragraph One</p>
-// <p>Paragraph Two</p>
-// <p>Paragraph Three</p>
+{/* <p>Paragraph One</p>
+<p>Paragraph Two</p>
+<p>Paragraph Three</p> */}
 // Define a new constant JSX that renders a div which contains the following elements in order:
 
 // An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
 
 // Note: When rendering multiple elements like this, you can wrap them all in parentheses, but it's not strictly required. Also notice this challenge uses a div tag to wrap all the child elements within a single parent element. If you remove the div, the JSX will no longer transpile. Keep this in mind, since it will also apply when you return JSX elements in React components.
+
+const JSX  = (
+  <div>
+    <h1>Hello</h1>
+    <p>Hello</p>
+    <ul>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+    </ul>
+  </div>
+)
+
+
+
 
 // 3.React: Add Comments in JSX
 // JSX is a syntax that gets compiled into valid JavaScript. Sometimes, for readability, you might need to add comments to your code. Like most programming languages, JSX has its own way to do this.
@@ -50,6 +65,7 @@ const JSX = <div></div>;
 // The code editor has a JSX element similar to what you created in the last challenge. Add a comment somewhere within the provided div element, without modifying the existing h1 or p elements.
 const JSX = (
     <div>
+    {/* Heres my comment */}
       <h1>This is a block of JSX</h1>
       <p>Here's a subtitle</p>
     </div>
@@ -61,7 +77,7 @@ const JSX = (
 
 //   4.React: Render HTML Elements to the DOM
 // So far, you've learned that JSX is a convenient tool to write readable HTML within JavaScript. With React, we can render this JSX directly to the HTML DOM using React's rendering API known as ReactDOM.
-
+                                                                                                              <App />, document.getElementById('root')
 // ReactDOM offers a simple method to render React elements to the DOM which looks like this: ReactDOM.render(componentToRender, targetNode), where the first argument is the React element or component that you want to render, and the second argument is the DOM node that you want to render the component to.
 
 // As you would expect, ReactDOM.render() must be called after the JSX element declarations, just like how you must declare variables before using them.
@@ -75,7 +91,7 @@ const JSX = (
     </div>
   );
   // Change code below this line
-
+ReactDOM.render(<JSX />, document.getElementById('challenge-node'));
 
   // 5 Define an HTML Class in JSX
 // Now that you're getting comfortable writing JSX, you may be wondering how it differs from HTML.
@@ -88,7 +104,7 @@ const JSX = (
 
 // Apply a class of myDiv to the div provided in the JSX code.
 const JSX = (
-  <div>
+  <div className='myDiv'>
     <h1>Add a class to this div</h1>
   </div>
 );
@@ -101,20 +117,21 @@ const JSX = (
 
 // In HTML, almost all tags have both an opening and closing tag: <div></div>; the closing tag always has a forward slash before the tag name that you are closing. However, there are special instances in HTML called “self-closing tags”, or tags that don’t require both an opening and closing tag before another tag can start.
 
-// For example the line-break tag can be written as <br> or as <br />, but should never be written as <br></br>, since it doesn't contain any content.
+// For example the line-break tag can be written as <br> or as <br />, but should never be written as <br>
+// </br>, since it doesn't contain any content.
 
 // In JSX, the rules are a little different. Any JSX element can be written with a self-closing tag, and every element must be closed. The line-break tag, for example, must always be written as <br /> in order to be valid JSX that can be transpiled. A <div>, on the other hand, can be written as <div /> or <div></div>. The difference is that in the first syntax version there is no way to include anything in the <div />. You will see in later challenges that this syntax is useful when rendering React components.
 
 // Fix the errors in the code editor so that it is valid JSX and successfully transpiles. Make sure you don't change any of the content - you only need to close tags where they are needed.
 
 
-// const JSX = (
-//   <div>
-//     <h2>Welcome to React!</h2> <br >
-//     <p>Be sure to close all tags!</p>
-//     <hr >
-//   </div>
-// );
+const JSX = (
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
 
  {/* 7 Create a Stateless Functional Component
 Components are the core of React. Everything in React is a component and here you will learn how to create one.
@@ -122,38 +139,43 @@ Components are the core of React. Everything in React is a component and here yo
 There are two ways to create a React component. The first way is to use a JavaScript function. Defining a component in this way creates a stateless functional component. The concept of state in an application will be covered in later challenges. For now, think of a stateless component as one that can receive data and render it, but does not manage or track changes to that data. (We'll cover the second way to create a React component in the next challenge.)
 
 To create a component with a function, you simply write a JavaScript function that returns either JSX or null. One important thing to note is that React requires your function name to begin with a capital letter. Here's an example of a stateless functional component that assigns an HTML class in JSX:
-
+ */}
 const DemoComponent = function() {
   return (
     <div className='customClass' />
   );
 };
-After being transpiled, the <div> will have a CSS class of customClass.
 
-Because a JSX component represents HTML, you could put several components together to create a more complex HTML page. This is one of the key advantages of the component architecture React provides. It allows you to compose your UI from many separate, isolated components. This makes it easier to build and maintain complex user interfaces.
+// After being transpiled, the <div> will have a CSS class of customClass.
 
-The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
+// Because a JSX component represents HTML, you could put several components together to create a more complex HTML page. This is one of the key advantages of the component architecture React provides. It allows you to compose your UI from many separate, isolated components. This makes it easier to build and maintain complex user interfaces.
 
-Note: The text is considered a child of the div element, so you will not be able to use a self-closing tag. */}
+// The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
+
+// Note: The text is considered a child of the div element, so you will not be able to use a self-closing tag. */}
 
 const MyComponent = function() {
- 
+  return (
+    <div>
+      Hello JSX
+    </div>
+  );
 }
 
 //8 Create a React Component
 // The other way to define a React component is with the ES6 class syntax. In the following example, Kitten extends React.Component:
 
-// class Kitten extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-//   render() {
-//     return (
-//       <h1>Hi</h1>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
 // This creates an ES6 class Kitten which extends the React.Component class. So the Kitten class now has access to many useful React features, such as local state and lifecycle hooks. Don't worry if you aren't familiar with these terms yet, they will be covered in greater detail in later challenges. Also notice the Kitten class has a constructor defined within it that calls super(). It uses super() to call the constructor of the parent class, in this case React.Component. The constructor is a special method used during the initialization of objects that are created with the class keyword. It is best practice to call a component's constructor with super, and pass props to both. This makes sure the component is initialized properly. For now, know that it is standard for this code to be included. Soon you will see other uses for the constructor as well as props.
 
 // MyComponent is defined in the code editor using class syntax. Finish writing the render method so it returns a div element that contains an h1 with the text Hello React!.
@@ -164,7 +186,11 @@ class MyComponent extends React.Component {
   }
   render() {
     // Change code below this line
-
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    )
 
 
     // Change code above this line
@@ -176,13 +202,13 @@ class MyComponent extends React.Component {
 
 // To compose these components together, you could create an App parent component which renders each of these three components as children. To render a component as a child in a React component, you include the component name written as a custom HTML tag in the JSX. For example, in the render method you could write:
 
-// return (
-//  <App>
-//   <Navbar />
-//   <Dashboard />
-//   <Footer />
-//  </App>
-// )
+return (
+ <App>
+  <Navbar />
+  <Dashboard />
+  <Footer />
+ </App>
+)
 // When React encounters a custom HTML tag that references another component (a component name wrapped in < /> like in this example), it renders the markup for that component in the location of the tag. This should illustrate the parent/child relationship between the App component and the Navbar, Dashboard, and Footer.
 
 // In the code editor, there is a simple functional component called ChildComponent and a class component called ParentComponent. Compose the two together by rendering the ChildComponent within the ParentComponent. Make sure to close the ChildComponent tag with a forward slash.
@@ -206,7 +232,7 @@ class ParentComponent extends React.Component {
       <div>
         <h1>I am the parent</h1>
         { /* Change code below this line */ }
-      
+        <ChildComponent />
 
         { /* Change code above this line */ }
       </div>
@@ -239,7 +265,7 @@ const Fruits = () => {
   return (
     <div>
       { /* Change code below this line */ }
-
+    <TypesOfFruit />
       { /* Change code above this line */ }
     </div>
   );
@@ -255,7 +281,7 @@ class TypesOfFood extends React.Component {
       <div>
         <h1>Types of Food:</h1>
         { /* Change code below this line */ }
-
+        <Fruits />
         { /* Change code above this line */ }
       </div>
     );
@@ -277,7 +303,8 @@ class Fruits extends React.Component {
       <div>
         <h2>Fruits:</h2>
         { /* Change code below this line */ }
-
+        <NonCitrus />
+        <Citrus />
         { /* Change code above this line */ }
       </div>
     );
@@ -293,7 +320,7 @@ class TypesOfFood extends React.Component {
       <div>
         <h1>Types of Food:</h1>
         { /* Change code below this line */ }
-
+        <Fruits />
         { /* Change code above this line */ }
         <Vegetables />
       </div>
@@ -318,7 +345,8 @@ class TypesOfFood extends React.Component {
       <div>
         <h1>Types of Food:</h1>
         {/* Change code below this line */}
-
+        <Fruits />
+        <Vegetables />
         {/* Change code above this line */}
       </div>
     );
@@ -326,7 +354,7 @@ class TypesOfFood extends React.Component {
 };
 
 // Change code below this line
-
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
 
 // 13 Write a React Component from Scratch
 // Now that you've learned the basics of JSX and React components, it's time to write a component on your own. React components are the core building blocks of React applications so it's important to become very familiar with writing them. Remember, a typical React component is an ES6 class which extends React.Component. It has a render method that returns HTML (from JSX) or null. This is the basic form of a React component. Once you understand this well, you will be prepared to start building more complex React projects.
@@ -334,7 +362,19 @@ class TypesOfFood extends React.Component {
 // Define a class MyComponent that extends React.Component. Its render method should return a div that contains an h1 tag with the text: My First React Component! in it. Use this text exactly, the case and punctuation matter. Make sure to call the constructor for your component, too.
 
 // Render this component to the DOM using ReactDOM.render(). There is a div with id='challenge-node' available for you to use.
-
+class MyComponent extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+          <div id="challenge-node">
+                 <h1>My First React Component!</h1>
+          </div>
+    );
+  }
+};
+ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
 
 
 // 14 Pass Props to a Stateless Functional Component
